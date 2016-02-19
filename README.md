41e1cdf469fe53a264d2934610367b9510a752c8
Felix Yan <286589258@qq.com>
2016-02-19 19:25:53 +1300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# GraphMA
+Automatic web service composition with graph-based Memetic algorithm
+# ECJ
---
+++ ec/graph/GraphEvol.java
@@ -0,0 +1,228 @@
+package ec.graph;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import ec.EvolutionState;
+import ec.Individual;
+import ec.Problem;
+import ec.simple.SimpleFitness;
+import ec.simple.SimpleProblemForm;
+import ec.util.Log;
+
+public class GraphEvol extends Problem implements SimpleProblemForm {
+
+	@Override
+	public void evaluate(EvolutionState state, Individual ind, int subpopulation, int threadnum) {
+		GraphInitializer init = (GraphInitializer) state.initializer;
+		if (init.runningOwls) {
+			evaluateOwls(init, state, ind, subpopulation, threadnum);
+		}
+		else {
+			evaluateQoS(init, state, ind, subpopulation, threadnum);
+		}
+	}
+
+	public void evaluateQoS(GraphInitializer init, EvolutionState state, Individual ind, int subpopulation, int threadnum) {
+		if (ind.evaluated) return;   //don't evaluate the individual if it's already evaluated
+		if (!(ind instanceof GraphIndividual))
+			state.output.fatal("Whoa!  It's not a GraphIndividual!!!",null);
+		GraphIndividual ind2 = (GraphIndividual)ind;
+
+		double a = 1.0;
+		double r = 1.0;
+		double t = 0.0;
+		double c = 0.0;
+
+		for (Node n : ind2.considerableNodeMap.values()) {
+			double[] qos = n.getQos();
+			a *= qos[GraphInitializer.AVAILABILITY];
+			r *= qos[GraphInitializer.RELIABILITY];
+			c += qos[GraphInitializer.COST];
+		}
+
+		// Calculate longest time
+		t = findLongestPath(ind2);
+
+		a = normaliseAvailability(a, init);
+		r = normaliseReliability(r, init);
+
+		//recalculate a and r with base e to make sure they are not too small
+		//a = Math.pow(Math.E*0.5, a-1);
+		//r = Math.pow(Math.E*0.5, r-1);
+		t = normaliseTime(t, init);
+		c = normaliseCost(c, init);
+
+		double fitness = init.w1 * a + init.w2 * r + init.w3 * t + init.w4 * c;
+
+		//System.out.println("maxA: "+init.maxAvailability+", maxR: "+init.maxReliability); //debug
+		//System.out.println("a: "+a+", r: "+r+", t: "+t+", c: "+c);//show values of 4 attributes, debug
+
+		((SimpleFitness)ind2.fitness).setFitness(state,
+				// ...the fitness...
+				fitness,
+				///... is the individual ideal?  Indicate here...
+				false);
+
+		ind2.evaluated = true;
+	}
+
+	public void evaluateOwls(GraphInitializer init, EvolutionState state, Individual ind, int subpopulation, int threadnum) {
+
+		if (ind.evaluated) return;   //don't evaluate the individual if it's already evaluated
+		if (!(ind instanceof GraphIndividual))
+			state.output.fatal("Whoa!  It's not a GraphIndividual!!!",null);
+		GraphIndividual ind2 = (GraphIndividual)ind;
+
+		// Calculate longest time
+		int runPath = findLongestPath2(ind2) - 1;
+		ind2.longestPathLength = runPath;
+		ind2.numAtomicServices = (ind2.considerableNodeMap.size() - 2);
+		boolean isIdeal = runPath == init.idealPathLength && ind2.numAtomicServices == init.idealNumAtomic;
+
+		double fitness = 0.5 * (1.0 / runPath) + 0.5 * (1.0/ ind2.numAtomicServices);
+		//double fitness = (100 - runPath) + (100 - ind2.numAtomicServices);
+
+		((SimpleFitness)ind2.fitness).setFitness(state,
+				// ...the fitness...
+				fitness,
+				///... is the individual ideal?  Indicate here...
+				isIdeal);
+
+		ind2.evaluated = true;
+	}
+
+	/*
+	 * The normalisation will maximize a and r, minimize t and c.
+	 */
+
+	private double normaliseAvailability(double availability, GraphInitializer init) {
+		if (init.maxAvailability - init.minAvailability == 0.0)
+			return 1.0;
+		else
+			return (availability - init.minAvailability)/(init.maxAvailability - init.minAvailability);
+	}
+
+	private double normaliseReliability(double reliability, GraphInitializer init) {
+		if (init.maxReliability - init.minReliability == 0.0)
+			return 1.0;
+		else
+			return (reliability - init.minReliability)/(init.maxReliability - init.minReliability);
+	}
+
+	private double normaliseTime(double time, GraphInitializer init) {
+		if (init.maxTime - init.minTime == 0.0)
+			return 1.0;
+		else
+			return (init.maxTime - time)/(init.maxTime - init.minTime);
+	}
+
+	private double normaliseCost(double cost, GraphInitializer init) {
+		if (init.maxCost - init.minCost == 0.0)
+			return 1.0;
+		else
+			return (init.maxCost - cost)/(init.maxCost - init.minCost);
+	}
+
+	/**
+	 * Uses the Bellman-Ford algorithm with negative weights to find the longest
+	 * path in an acyclic directed graph. Length is calculated by the sum of execution time.
+	 *
+	 * @param g
+	 * @return list of edges composing longest path
+	 */
+	private double findLongestPath(GraphIndividual g) {
+		Map<String, Double> distance = new HashMap<String, Double>();
+		Map<String, Node> predecessor = new HashMap<String, Node>();
+
+		// Step 1: initialize graph
+		for (Node node : g.considerableNodeMap.values()) {
+			if (node.getName().equals("start"))
+				distance.put(node.getName(), 0.0);
+			else
+				distance.put(node.getName(), Double.POSITIVE_INFINITY);
+		}
+
+		// Step 2: relax edges repeatedly
+		for (int i = 1; i < g.considerableNodeMap.size(); i++) {
+			for (Edge e : g.considerableEdgeList) {
+				//debug
+				if(!distance.containsKey(e.getFromNode().getName())){
+					System.out.println("ToNode=="+e.getToNode().getName());
+					System.out.println("FromNode=="+e.getFromNode().getName());
+				}
+
+				if ((distance.get(
+						e.getFromNode()
+						.getName()) -
+						e.getToNode().getQos()[GraphInitializer.TIME])
+						< distance.get(e.getToNode().getName())) {
+					distance.put(e.getToNode().getName(), (distance.get(e.getFromNode().getName()) - e.getToNode().getQos()[GraphInitializer.TIME]));
+					predecessor.put(e.getToNode().getName(), e.getFromNode());
+				}
+			}
+		}
+
+		// Now retrieve total cost
+		Node pre = predecessor.get("end");
+		double totalTime = 0.0;
+
+		while (pre != null) {
+			totalTime += pre.getQos()[GraphInitializer.TIME];
+			pre = predecessor.get(pre.getName());
+		}
+
+		return totalTime;
+	}
+
+	/**
+	 * Uses the Bellman-Ford algorithm with negative weights to find the longest
+	 * path in an acyclic directed graph. Length is calculated by the number of nodes.
+	 *
+	 * @param g
+	 * @return list of edges composing longest path
+	 */
+	private int findLongestPath2(GraphIndividual g) {
+		Map<String, Integer> distance = new HashMap<String, Integer>();
+		Map<String, Node> predecessor = new HashMap<String, Node>();
+
+		// Step 1: initialize graph
+		for (Node node : g.considerableNodeMap.values()) {
+			if (node.getName().equals("start"))
+				distance.put(node.getName(), 0);
+			else
+				distance.put(node.getName(), Integer.MAX_VALUE);
+		}
+
+		// Step 2: relax edges repeatedly
+		for (int i = 1; i < g.considerableNodeMap.size(); i++) {
+			for (Edge e : g.considerableEdgeList) {
+				if ((distance.get(e.getFromNode().getName()) - 1)
+						< distance.get(e.getToNode().getName())) {
+					distance.put(e.getToNode().getName(), (distance.get(e.getFromNode().getName()) - 1));
+					predecessor.put(e.getToNode().getName(), e.getFromNode());
+				}
+			}
+		}
+
+		// Now retrieve total cost
+		Node pre = predecessor.get("end");
+		int totalTime = 0;
+		while (pre != null) {
+			totalTime += 1;
+			pre = predecessor.get(pre.getName());
+		}
+
+		return totalTime;
+	}
+
+	//	@Override
+	//	public void describe(EvolutionState state, Individual ind, int subpopulation, int thread, int log) {
+	//		Log l = state.output.getLog(log);
+	//		GraphIndividual graph = (GraphIndividual) ind;
+	//
+	//		System.out.println(String.format("runPath= %d #atomicProcess= %d\n", graph.longestPathLength, graph.considerableNodeMap.size() - 2));
+	//		l.writer.append(String.format("runPath= %d #atomicProcess= %d\n", graph.longestPathLength, graph.considerableNodeMap.size() - 2));
+	//	}
+}
